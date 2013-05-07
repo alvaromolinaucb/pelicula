21fa09191b056680740754c05bd004c5a8f39d00
Miguel Alvaro Molina <alvaromolinac@gmail.com>
2013-05-07 13:17:12 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pelicula::Application.config.secret_token = 'd0e07836cbffb2c151d28bb76aad21e63487951ec5c242f7e7425935ebddec6ded732496c3bbbd1d0d94f8529fb3539dc47347847991a0cb9556c32f227d8d95'
